c4be066fba0b4d3d4c7f18946c1d48527fa52793
mmaxson <mmurun@gmail.com>
2018-04-24 20:29:34 -0700

    Validate properties.

--- src/main/java/com/murun/fstats/main/ConfigProperties.java
+++ src/main/java/com/murun/fstats/main/ConfigProperties.java
@@ -5,13 +5,17 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 @Configuration
 @PropertySource("application.properties")
 @ConfigurationProperties(prefix = "app")
 public class ConfigProperties {
 
 
-
+    @Min(1)
+    @Max(32767)
     private short maxDataLines;
 
     public short getMaxDataLines() {
